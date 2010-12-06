ceaf3af423eca5047a02fb9da07e2352c65d4564
Mark McGranaghan <mmcgrana@gmail.com>
2010-12-06 09:21:41 -0800

    release 0.3.5

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ring/ring-httpcore-adapter "0.3.4"
+(defproject ring/ring-httpcore-adapter "0.3.5"
   :description "Ring HttpCore adapter."
   :url "http://github.com/mmcgrana/ring-httpcore-adapter"
-  :dependencies [[ring/ring-core "0.3.4"]
+  :dependencies [[ring/ring-core "0.3.5"]
                  [org.apache.httpcomponents/httpcore "4.0.1"]
                  [org.apache.httpcomponents/httpcore-nio "4.0.1"]])
